80dec4e243244fd0cf7cc90dce5f0e8882b04dd0
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-09-13 06:20:39 +0900

    refoctoring

--- cal.rb
+++ cal.rb
@@ -33,19 +33,41 @@ days = []
 # 週ごとに配列を分ける
 weekdays = days.each_slice(7).to_a
 
+# 表示に関するものをメソッド化
+def arrange_to_three_chars(day)
+  if day.to_i < 10
+    " #{day} "
+  else
+    "#{day} "
+  end
+end
+
+def black_and_white_reversal(today)
+  "\e[7;37m#{today}\e[0m "
+end
+
+def today?(year, month, day)
+  today = Date.today
+  today.year == year &&
+  today.month == month &&
+  today.day == day
+end
+
+def enter_new_line
+  puts "\n"
+end
+
 # カレンダーの表示
 puts "      #{month}月 #{year}    "
 puts "日 月 火 水 木 金 土"
 weekdays.each do |week|
   week.each do |day|
-    if day == today.day && month == today.month && year == today.year
-      print "\e[7;37m#{today.day}\e[0m "
-    elsif day.to_i< 10
-      print " #{day} "
+    if today?(year, month, day)
+      print black_and_white_reversal(day)
     else
-      print "#{day} "
+      print arrange_to_three_chars(day)
     end
   end
-  puts "\n"
+  enter_new_line
 end
-puts "\n"
+enter_new_line
