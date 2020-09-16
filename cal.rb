e9d9f7cb46f5e1f0008b7b82caad2631838fd22b
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-09-16 18:21:32 +0900

    run rubocop

--- cal.rb
+++ cal.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'optparse'
 require 'date'
 
-# 表示に関するメソッド
+# methods for display
 def arrange_to_three_chars(day)
   day.to_i < 10 ? " #{day} " : "#{day} "
 end
@@ -13,28 +15,27 @@ end
 
 def today?(today, year, month, day)
   year, month, day = [year, month, day].map!(&:to_i)
-  today == Date.new(year, month, day) unless day == 0
+  today == Date.new(year, month, day) unless day.zero?
 end
 
 today = Date.today
-# コマンドライン引数を変数化
-options = ARGV.getopts("m:", "y:")
+options = ARGV.getopts('m:', 'y:')
 
-# 月の設定
-month = options["m"] || today.month
-# 年の設定
-year = options["y"] || today.year
+# variable for month
+month = options['m'] || today.month
+# variable for year
+year = options['y'] || today.year
 
-# 月初月末の変数化
-first_day = Date.new(year, month , 1)
-last_day = Date.new(year, month , -1)
+# variable for day
+first_day = Date.new(year, month, 1)
+last_day = Date.new(year, month, -1)
 
-# カレンダーに使用する配列を作成
-days = ([" "] * first_day.wday) + (1..last_day.day).to_a
+# array for calendar
+days = ([' '] * first_day.wday) + (1..last_day.day).to_a
 
-# カレンダーの表示
+# display calendar
 puts "      #{month}月 #{year}    "
-puts "日 月 火 水 木 金 土"
+puts '日 月 火 水 木 金 土'
 days.each_slice(7) do |week|
   week.each do |day|
     if today?(today, year, month, day)
--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,16 @@
 #!/usr/bin/env ruby
-# 1から100までの数をプリントするプログラムを書け。ただし3の倍数のときは数の代わりに｢Fizz｣と、5の倍数のときは｢Buzz｣とプリントし、3と5両方の倍数の場合には｢FizzBuzz｣とプリントすること。
+# frozen_string_literal: true
 
 def fizz_buzz(int)
-  case
-  when int % 15 == 0
-    "FizzBuzz"
-  when int % 3 == 0
-    "Fizz"
-  when int % 5 == 0
-    "Buzz"
+  if (int % 15).zero?
+    'FizzBuzz'
+  elsif (int % 3).zero?
+    'Fizz'
+  elsif (int % 5).zero?
+    'Buzz'
   else
     int
   end
 end
 
-1.upto(100){ |int| puts fizz_buzz(int) }
+1.upto(100) { |int| puts fizz_buzz(int) }
