4f994d21d01189df5a23eeb857bfd0ce4c3b734d
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-09-17 16:44:32 +0900

    finish refoctoring

--- bowling.rb
+++ bowling.rb
@@ -1,81 +1,67 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-score = ARGV[0]
-scores = score.chars
+string_score = ARGV[0]
+scores = string_score.chars
 
 frames = Array.new(9) { [0, 0] }
 frames.push([0, 0, 0])
 
-count = 0
-first_throw = true
-final_flag = false
+frame_count = 0
+frame_first_throw = true
+final_throw = false
+
+def to_integer(score)
+  score == 'X' ? 10 : score.to_i
+end
+
 scores.each do |score|
-  if count == 9 && first_throw
-    if score == "X"
-      frames[9][0] = 10
-      first_throw = false
-    else
-      frames[9][0] = score.to_i
-      first_throw = false
-    end
-  elsif count == 9 && !final_flag
-    if score == "X"
-      frames[9][1] = 10
-    else
-      puts "きたよ"
-      frames[9][1] = score.to_i
-    end
-    final_flag = true
-  elsif final_flag
+  if frame_count == 9 && frame_first_throw
+    frames[9][0] = to_integer(score)
+    frame_first_throw = false
+  elsif frame_count == 9 && !final_throw
+    frames[9][1] = to_integer(score)
+    final_throw = true
+  elsif final_throw
     if frames[9].sum >= 10
-      if score == "X"
-        frames[9][2] = 10
-      else
-        frames[9][2] = score.to_i
-      end
+      frames[9][2] = to_integer(score)
+    end
+  elsif frame_first_throw
+    frames[frame_count][0] = to_integer(score)
+    case score
+    when 'X'
+      frames[frame_count][1] = 0
+      frame_count += 1
+      frame_first_throw = true
+    else
+      frame_first_throw = false
     end
   else
-    if first_throw
-      if score == "X"
-        frames[count][0] = 10
-        frames[count][1] = 0
-        count += 1
-        first_throw = true
-      else
-        frames[count][0] = score.to_i
-        first_throw = false
-      end
-    else
-      if score == "X"
-        frames[count][1] = 10
-      else
-        frames[count][1] = score.to_i
-      end
-      count += 1
-      first_throw = true
-    end
+    frames[frame_count][1] = to_integer(score)
+    frame_count += 1
+    frame_first_throw = true
   end
 end
 
 point = 0
 frames.each_with_index do |frame, idx|
-  if idx == 9
-    p point += frame.sum
-  elsif frame[0] == 10
-    if idx == 8
-      p point += (10 + frames[9][0] + frames[9][1])
-    elsif frames[idx + 1][0] == 10
-      p point += (10 + 10 + frames[idx + 2][0])
+  point +=
+    case
+    when idx == 9
+      frame.sum
+    when idx == 8 && frame[0] == 10
+      10 + frames[9][0] + frames[9][1]
+    when frame[0] == 10
+      if frames[idx + 1][0] == 10
+        10 + 10 + frames[idx + 2][0]
+      else
+        10 + frames[idx + 1].sum
+      end
+    when frame.sum >= 10
+      10 + frames[idx + 1][0]
     else
-      p point += (10 + frames[idx + 1].sum)
+      frame.sum
     end
-  elsif frame.sum >= 10 && idx != 9
-    p point += (10 + frames[idx + 1][0])
-  else
-    p point += frame.sum
-  end
 end
 
 puts point
-
