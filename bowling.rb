24ba84e06f02394319358c1c4be827f242b10ade
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-09-18 18:03:29 +0900

    refoctoring after 2nd reviewed

--- bowling.rb
+++ bowling.rb
@@ -1,15 +1,65 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-string_score = ARGV[0]
-scores = string_score.chars
+def main(string_score)
+  scores = string_score.chars
 
-frames = Array.new(9) { [0, 0] }
-frames.push([0, 0, 0])
+  frames = Array.new(9) { [0, 0] }
+  frames.push([0, 0, 0])
 
-frame_count = 0
-frame_first_throw = true
-final_throw = false
+  frame_count = 0
+  frame_first_throw = true
+  final_throw = false
+
+  scores.each do |score_char|
+    score = to_integer(score_char)
+    if final_frame?(frame_count)
+      if frame_first_throw
+        frames[9][0] = score
+        frame_first_throw = false
+      elsif !final_throw
+        frames[9][1] = score
+        final_throw = true
+      else
+        frames[9][2] = score
+      end
+    elsif frame_first_throw
+      frames[frame_count][0] = score
+      case score
+      when 10
+        frame_count += 1
+        frame_first_throw = true
+      else
+        frame_first_throw = false
+      end
+    else
+      frames[frame_count][1] = score
+      frame_count += 1
+      frame_first_throw = true
+    end
+  end
+
+  point = frames.each_with_index.sum do |this_frame, this_time|
+    next_time = this_time + 1
+    next_frame = frames[next_time]
+    case
+    when final_frame?(this_time)
+      this_frame.sum
+    when final_frame?(next_time) && strike?(this_frame)
+      10 + next_frame[0..1].sum
+    when strike?(this_frame)
+      if strike?(next_frame)
+        10 + 10 + frames[next_time + 1][0]
+      else
+        10 + next_frame.sum
+      end
+    when spare?(this_frame)
+      10 + frames[next_time][0]
+    else
+      this_frame.sum
+    end
+  end
+end
 
 def to_integer(score)
   score == 'X' ? 10 : score.to_i
@@ -27,52 +77,4 @@ def spare?(frame)
   frame.sum == 10 && !strike?(frame)
 end
 
-scores.each do |score|
-  if final_frame?(frame_count)
-    if frame_first_throw
-      frames[9][0] = to_integer(score)
-      frame_first_throw = false
-    elsif !final_throw
-      frames[9][1] = to_integer(score)
-      final_throw = true
-    elsif final_throw
-      frames[9][2] = to_integer(score)
-    end
-  elsif frame_first_throw
-    frames[frame_count][0] = to_integer(score)
-    case score
-    when 'X'
-      frame_count += 1
-      frame_first_throw = true
-    else
-      frame_first_throw = false
-    end
-  else
-    frames[frame_count][1] = to_integer(score)
-    frame_count += 1
-    frame_first_throw = true
-  end
-end
-
-point = 0
-point = frames.each_with_index.sum do |frame, idx|
-  next_frame = frames[idx + 1]
-  case
-  when idx == 9
-    frame.sum
-  when idx == 8 && strike?(frame)
-    10 + frames[9][0] + frames[9][1]
-  when strike?(frame)
-    if strike?(next_frame)
-      10 + 10 + frames[idx + 2][0]
-    else
-      10 + next_frame.sum
-    end
-  when spare?(frame)
-    10 + next_frame[0]
-  else
-    frame.sum
-  end
-end
-
-puts point
+puts main(ARGV[0])
