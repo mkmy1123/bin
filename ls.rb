8de402b8209a96d9ebdaede355235f00c1c54277
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-09-18 12:34:45 +0900

    ls command / implement with argument

--- ls.rb
+++ ls.rb
@@ -5,18 +5,28 @@ require 'optparse'
 
 options = ARGV.getopts('a', 'r', 'l')
 results = []
-
-if options['a']
+file_or_dir = ARGV
+# error = proc { raise ArgumentError, "ls: #{file_or_dir}: No such file or directory" }
+if file_or_dir != []
+  file_or_dir.each do |fod|
+    if Dir.exist?(fod)
+      Dir.open(fod).each_child { |f| results << f.ljust(11) unless f[0] == "." }
+      results.sort!
+    elsif File.expand_path(fod) && File.expand_path(fod) != (Dir.pwd + '/' + fod)
+      results << File.expand_path(fod)
+    else
+      Dir.open('.').each { |f| results << f.ljust(11) if fod == f }
+    end
+  end
+  results << "\n"
+elsif options['a']
   Dir.open('.').each { |f| results << f.ljust(11) }
   results.sort!
-  results << "\n"
 elsif options['r']
   Dir.open('.').each_child { |f| results << f.ljust(11) unless f[0] == "." }
   results.sort!.reverse!
-  results << "\n"
 else
   Dir.open('.').each_child { |f| results << f.ljust(11) unless f[0] == "." }
   results.sort!
-  results << "\n"
 end
 print results.join(' ')
